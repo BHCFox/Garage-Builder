bcda265eb1bf5ed5b8ba76a3ca5ff7fd029a7488
BHCFox <207841785+BHCFox@users.noreply.github.com>
2026-03-21 02:10:39 +1100

    Added form data validation, storing data in memory and added dashboard for car history

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-const form = document.getElementById("carForm");
+const cars = []; // This is our main storage — an empty array to keep all cars the user adds
 
-form.addEventListener("submit", function (e) {
-  e.preventDefault(); // Stops form from refreshing the page
+const form = document.getElementById("carForm"); // Grab the form element from the HTML using its id
 
+// User Input for Submit Button
+form.addEventListener("submit", function (event) {
+  //event is just parameter name
+  event.preventDefault(); // Stops page refresh
+
+  // Get the value typed into the "make" input field
   const make = document.getElementById("make").value;
   const model = document.getElementById("model").value;
   const year = document.getElementById("year").value;
+  const category = document.getElementById("category").value;
 
-  const car = { make, model, year }; // put all the values into car variable
+  // Create a car object using the values collected above
+  const car = { make, model, year, category };
 
+  if (category === "") {
+    alert("Please select a Category");
+    return;
+  }
+
+  if (make === "" || model === "") {
+    alert("Please fill in all fields..");
+    return;
+  }
+
+  // Add this new car object into our cars array (store it)
   cars.push(car);
 
+  // Log the updated cars array to the console so we can see what's happening
   console.log(cars);
-});
 
-const cars = []; //creates the storage for new inputs
+  form.reset();
+});
